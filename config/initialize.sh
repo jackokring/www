c1a91f0c4a8587c499f250927a6189336b263bb4
jackokring <jackokring@gmail.com>
2022-10-08 14:11:45 +0100

    Background tasking

--- config/initialize.sh
+++ config/initialize.sh
@@ -18,7 +18,7 @@ sudo systemctl restart apache2
 # start node :3000
 cd node
 npm install express jquery bootstrap underscore
-node app.js
+node app.js&
 
 # python install :8000
 # and a wolfram kernal :18000
@@ -26,8 +26,10 @@ node app.js
 pip install autograd wolframwebengine jax[cpu]
 # full www directory. Not sutable for production
 # and cgi-bin handler for localhost use
-python3 -m http.server --cgi
+python3 -m http.server --cgi&
 # wolfram web engine licence (V13.1)
 sudo cp secrets/LICENSE.txt /usr/local/Wolfram/WolframEngine/13.1
-python3 -m wolframwebengine wl
+python3 -m wolframwebengine wl&
 
+# so then launch browser default
+xdg-open "http://localhost"
