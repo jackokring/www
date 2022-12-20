ea9275856e5908a619403efe33504e465bd70011
jackokring <jackokring@gmail.com>
2022-12-20 13:31:53 +0000

    directory

--- cron-monthly.sh
+++ cron-monthly.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # monthly tasks
 
+# run as $USER
+# don't config-env.sh as it is not command initiated, but monthly
+# this means the $PATH is set via cron, and not all commands will work as expected
+# but a logical directory is ~ and as might depend on interactive then
+cd ~
+
 # update certbot for SSL
 sudo /opt/certbot/bin/pip install --upgrade certbot
-
-# script directory
-DIR=$( dirname -- "$0"; )
-cd "$DIR"
 
 notify-send "phinka cron monthly complete"
