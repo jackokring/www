361fc78769cec5c398956c06f57372f003a73383
jackokring <jackokring@gmail.com>
2022-12-31 18:44:11 +0000

    shellinabox

---
+++ siab.py
@@ -0,0 +1,6 @@
+# SHELLINABOX_ARGS="-s '/:jackokring:jackokring:HOME:python www/siab.py'"
+# >> /etc/default/shellinabox
+# use curses module wrapper for terminal GUI
+# requires .pem to avoid cert self sign error
+
+print('hi')
