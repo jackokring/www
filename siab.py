01564995d3395572e609a8130e3db834e36e1c50
jackokring <jackokring@gmail.com>
2023-01-01 22:26:56 +0000

    url

--- siab.py
+++ siab.py
@@ -3,4 +3,7 @@
 # use curses module wrapper for terminal GUI
 # requires .pem to avoid cert self sign error
 
+# multiple routes beyond / and exit/error terminates shell connect
+# $USER, https://localhost:4200/
+
 print('hi')
