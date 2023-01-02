09c725af57e7e61a4cdbb3023dc688691ba431ad
jackokring <jackokring@gmail.com>
2023-01-02 12:27:58 +0000

    basic curses wrapper

--- siab.py
+++ siab.py
@@ -6,4 +6,17 @@
 # multiple routes beyond / and exit/error terminates shell connect
 # $USER, https://localhost:4200/
 
-print('hi')
+from curses import wrapper
+
+def main(stdscr):
+    # Clear screen
+    stdscr.clear()
+
+    for i in range(0, 11):
+        v = i-12
+        stdscr.addstr(i, 0, '10 divided by {} is {}'.format(v, 10/v))
+
+    stdscr.refresh()
+    stdscr.getkey()
+
+wrapper(main)
